39fa9995cef7667d78433a6cb854871938ae15bf
Leo Borquez <ele.dev@protonmail.com>
2020-04-23 23:17:58 -0400

    connection to database

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,9 @@ module github.com/LeoBorquez/worki
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.16 // indirect
+require (
+	github.com/go-pg/pg v8.0.6+incompatible
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/labstack/echo/v4 v4.1.16
+	mellium.im/sasl v0.2.1 // indirect
+)
--- backend/server.go
+++ backend/server.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-pg/pg"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+
+	connect()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Test Echo!")
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+func connect() {
+	db := pg.Connect(&pg.Options{
+		User:     "postgres",
+		Password: "password",
+		Database: "worki",
+	})
+
+	if db == nil {
+		log.Printf("Failed to connect to database.\n")
+		os.Exit(100)
+	}
+	log.Printf("Connected succesfully")
+
+	defer db.Close()
 }
