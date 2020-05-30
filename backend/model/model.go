d45e59ee871adfa04a5d6006e1104adac879add7
Leo Borquez <leoborquezg@protonmail.com>
2020-05-30 00:12:25 -0400

    modification connection string

--- backend/model/model.go
+++ backend/model/model.go
@@ -2,32 +2,42 @@ package model
 
 import (
 	"fmt"
-	"log"
+	"os"
 
-	pg "github.com/go-pg/pg"
-	_ "github.com/joho/godotenv"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
+
+var db *gorm.DB
+
+func init() {
+	e := godotenv.Load()
+	if e != nil {
+		fmt.Print(e)
+	}
+	username := os.Getenv("db_user")
+	//password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", dbHost, username, dbName)
+	fmt.Println(dbURI)
+
+	conn, err := gorm.Open("postgres", dbURI)
+	if err != nil {
+		fmt.Print(err)
+	}
+	db = conn
+	db.Debug().AutoMigrate()
+}
+
+// returns a handle to the DB object
+func GetDB() *gorm.DB {
+	return db
+}
 
 // TestExport testing export
 func TestExport() {
 	fmt.Println("test export")
-}
-
-// CreateCon create the connection to th db
-func CreateCon() {
-	db := pg.Connect(&pg.Options{
-		User:     "postgres",
-		Password: "password",
-		Database: "worki",
-	})
-
-	if db == nil {
-		log.Panic("Failed to connect to database.\n")
-	}
-	log.Printf("Connected succesfully")
-
-	// migration
-	CreateGigTable(db)
-
-	defer db.Close()
 }
