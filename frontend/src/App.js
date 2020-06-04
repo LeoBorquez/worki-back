8b194459dfc39d531b3456043f653e2b9378661b
ncriis <cristobal.salinasg@gmail.com>
2020-06-03 23:07:13 -0400

    Carga front worki

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo-worki.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PRONTO
+        </p>
+        <p>Portal de empleos para personas <code>independientes</code>
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://forms.gle/YDw5u5r7yXrBhNGJ6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Más información
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
