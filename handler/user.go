89779c727d033183e50a8c39b0dab34a492ff99a
Leo Borquez <leoborquezg@protonmail.com>
2020-06-21 18:43:22 -0400

    ADD: configuration server

--- handler/user.go
+++ handler/user.go
@@ -9,17 +9,6 @@ import (
 	"github.com/labstack/echo"
 )
 
-/*
-type UserResquest struct {
-	Name     string `sql:"name"`
-	LastName string `sql:"last_name"`
-	Email    string `sql:"email"`
-	Phone    string `sql:"phone"`
-	Pasword  string `sql:"password"`
-	Toke     string `sql:"token"`
-}
-*/
-
 // Signup user handler
 func (h *Handler) Signup(c echo.Context) (err error) {
 	// Bind
@@ -53,7 +42,7 @@ func (h *Handler) Login(c echo.Context) (err error) {
 	// Find user
 	db := h.DB
 	defer db.Close()
-	if err = db.Table("users").Find(u, "email = ? and password = ?", u.Email, u.Password).Error; err != nil {
+	if err = db.Table("users").Find(u, "email = ? and password = ?", u.Email, u.Password).Error; err == nil {
 		return &echo.HTTPError{Code: http.StatusUnauthorized, Message: "invalid email or password"}
 	}
 
