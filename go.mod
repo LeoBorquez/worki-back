3444462ecaad5ee5a0f63d2dcb5827ba0bcdc673
Leo Borquez <15177945+LeoBorquez@users.noreply.github.com>
2021-02-18 23:28:10 -0300

    🐛 FIX: mock db

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/LeoBorquez/worki-back
 go 1.15
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/biezhi/gorm-paginator/pagination v0.0.0-20190124091837-7a5c8ed20334
 	github.com/brianvoe/gofakeit v3.18.0+incompatible
 	github.com/cespare/reflex v0.3.0 // indirect
--- handler/user_test.go
+++ handler/user_test.go
@@ -1,31 +1,41 @@
 package handler
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
-	"github.com/LeoBorquez/worki-back/model"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	mockDB = map[string]*model.CreateUser{
-		"user1": &model.CreateUser{Email: "leoborquez@mail.com", Password: "123123123"},
-	}
-	userJSON = `{"Email":"jon@labstack.com", "Password":"testing"}`
+	mock     sqlmock.Sqlmock
+	userJSON = `{"Email":"leoborquez@mail.com","Password":"123455"}`
+	db       *sql.DB
+	err      error
 )
 
 func TestCreateUser(t *testing.T) {
 	//Setup
+
+	db, mock, err = sqlmock.New()
+	gdb, err := gorm.Open("postgres", db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJSON))
+	req := httptest.NewRequest(http.MethodPost, "/signup", strings.NewReader(userJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
-	h := &Handler{}
+	h := &Handler{gdb}
 
 	// Assertions
 	if assert.NoError(t, h.Signup(c)) {
