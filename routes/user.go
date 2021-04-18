7551259c9f9b1a8b068c7ba47c9000aeaae5238f
Leo Borquez <15177945+LeoBorquez@users.noreply.github.com>
2021-04-17 23:02:51 -0400

    👌 IMPROVE: routes

--- routes/user.go
+++ routes/user.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/LeoBorquez/worki-back/handler"
+	"github.com/labstack/echo"
+)
+
+func UserRoute(e *echo.Echo, h *handler.Handler) {
+	e.POST("/signup", h.SignUp)
+}
